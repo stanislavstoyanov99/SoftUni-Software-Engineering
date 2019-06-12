793d86b8b441a560c6da4b704a9b58759e51e0cd
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-12 22:54:34 +0300

    Added multidimensional arrays lab

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/MultidimensionalArraysLab/07PascalTriangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _07PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long rows = int.Parse(Console.ReadLine());
+
+            long[][] jaggedArray = new long[rows][];
+            int cols = 1;
+
+            for (int row = 0; row < rows; row++)
+            {
+                jaggedArray[row] = new long[cols];
+                jaggedArray[row][0] = 1;
+                jaggedArray[row][cols - 1] = 1;
+
+                if (cols > 2)
+                {
+                    long[] previousRow = jaggedArray[row - 1];
+
+                    for (int col = 1; col < cols - 1; col++)
+                    {
+                        jaggedArray[row][col] = previousRow[col] + previousRow[col - 1];
+                    }
+                }
+
+                cols++;
+            }
+
+            foreach (var item in jaggedArray)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+        }
+    }
+}
