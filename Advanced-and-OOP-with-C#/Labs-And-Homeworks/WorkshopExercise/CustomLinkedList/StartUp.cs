99e7f9c2a873d801a8a0939b3076961be88b8ea7
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-13 19:47:47 +0300

    Added workshop exercise

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/WorkshopExercise/CustomLinkedList/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomLinkedList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var doublyLinkedList = new DoublyLinkedList<string>();
+
+            doublyLinkedList.AddLast("Pesho");
+            doublyLinkedList.AddLast("Gosho");
+            doublyLinkedList.AddLast("Kiro");
+
+            doublyLinkedList.Print(Console.WriteLine);
+            string[] array = doublyLinkedList.ToArray();
+
+            Console.WriteLine(doublyLinkedList.Contains("Gosho"));
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/WorkshopExercise/LaptopShop/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace LaptopShop
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
