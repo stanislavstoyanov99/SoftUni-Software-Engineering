d90c69fac637e1bae4f141109482779daca91b4c
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-13 21:41:35 +0300

    Added stacks and queues lab

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/StacksAndQueuesLab/02StackSum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()).ToLower() != "end")
+            {
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0].ToLower();
+
+                switch (command)
+                {
+                    case "add":
+                        int firstNumberToAdd = int.Parse(tokens[1]);
+                        int secondNumberToAdd = int.Parse(tokens[2]);
+
+                        stack.Push(firstNumberToAdd);
+                        stack.Push(secondNumberToAdd);
+                        break;
+                    case "remove":
+                        int numbersToRemove = int.Parse(tokens[1]);
+
+                        if (numbersToRemove > stack.Count)
+                        {
+                            break;
+                        }
+
+                        for (int i = 0; i < numbersToRemove; i++)
+                        {
+                            stack.Pop();
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/StacksAndQueuesLab/6HotPotato/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> queue = new Queue<string>(input);
+
+            int n = int.Parse(Console.ReadLine());
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    string player = queue.Dequeue();
+                    queue.Enqueue(player);
+                }
+
+                string removedPerson = queue.Dequeue();
+                Console.WriteLine($"Removed {removedPerson}");
+            }
+
+            string lastPerson = queue.Dequeue();
+            Console.WriteLine($"Last is {lastPerson}");
+        }
+    }
+}
