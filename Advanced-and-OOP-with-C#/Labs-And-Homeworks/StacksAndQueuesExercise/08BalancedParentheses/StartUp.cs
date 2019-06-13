640179d381505da3e091771ffffc95d2019b21e1
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-13 21:41:04 +0300

    Added stacks and queues exercise

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/StacksAndQueuesExercise/08BalancedParentheses/StartUp.cs
@@ -0,0 +1,61 @@
+﻿namespace _08BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stackOfParanteses = new Stack<char>();
+            char[] input = Console.ReadLine().ToCharArray();
+
+            char[] openParaneteses = new char[] { '(', '{', '[' };
+
+            bool isValid = true;
+
+            foreach (var item in input)
+            {
+                if (openParaneteses.Contains(item))
+                {
+                    stackOfParanteses.Push(item);
+                    continue;
+                }
+
+                if (stackOfParanteses.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+
+                if (stackOfParanteses.Peek() == '(' && item == ')')
+                {
+                    stackOfParanteses.Pop();
+                }
+                else if (stackOfParanteses.Peek() == '[' && item == ']')
+                {
+                    stackOfParanteses.Pop();
+                }
+                else if (stackOfParanteses.Peek() == '{' && item == '}')
+                {
+                    stackOfParanteses.Pop();
+                }
+                else
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/StacksAndQueuesExercise/12CupsAndBottles/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12CupsAndBottles
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            int[] cupsCapacity = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] filledBottles = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queueOfCups = new Queue<int>(cupsCapacity);
+            Stack<int> stackOfBottles = new Stack<int>(filledBottles);
+
+            int wastedWater = 0;
+
+            while (queueOfCups.Count > 0 && stackOfBottles.Count > 0)
+            {
+                int currentCup = queueOfCups.Peek();
+                int currentBottle = stackOfBottles.Peek();
+
+                if (currentBottle >= currentCup)
+                {
+                    queueOfCups.Dequeue();
+                    wastedWater += Math.Abs(currentBottle - currentCup);
+                    stackOfBottles.Pop();
+                }
+                else
+                {
+                    while (true)
+                    {
+                        if (currentCup - currentBottle <= 0)
+                        {
+                            queueOfCups.Dequeue();
+                            wastedWater += Math.Abs(currentBottle - currentCup);
+                            stackOfBottles.Pop();
+                            break;
+                        }
+                        else
+                        {
+                            currentCup -= currentBottle;
+                            stackOfBottles.Pop();
+                            currentBottle = stackOfBottles.Peek();
+                        }
+                    }
+                }
+            }
+
+            if (stackOfBottles.Count > 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", stackOfBottles)}");
+            }
+            else
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", queueOfCups)}");
+            }
+
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
