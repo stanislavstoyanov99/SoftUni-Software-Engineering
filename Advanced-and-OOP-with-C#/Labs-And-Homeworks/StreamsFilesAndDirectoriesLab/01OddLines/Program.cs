68e7ea3b1750d8d94c7f4c142ca81cea6dacb8f2
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-14 10:11:14 +0300

    Added streams files and directories lab

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/StreamsFilesAndDirectoriesLab/01OddLines/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace _01OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader(@"files\input.txt");
+
+            using (reader)
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+
+                using (var writer = new StreamWriter(@"files\output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/StreamsFilesAndDirectoriesLab/02LineNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _02LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader(@"files\input.txt");
+
+            using (reader)
+            {
+                int counter = 1;
+                string line = reader.ReadLine();
+
+                using (var writer = new StreamWriter(@"files\output.txt"))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
