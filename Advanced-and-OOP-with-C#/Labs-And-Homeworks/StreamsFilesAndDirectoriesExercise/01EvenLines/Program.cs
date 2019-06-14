30b83e872d0249043c628ecbad9099f38e57acb8
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-14 10:10:43 +0300

    Added streams files and directories exercise

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/StreamsFilesAndDirectoriesExercise/01EvenLines/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader(@"files\input.txt");
+
+            using (reader)
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+
+                string[] symbolsToReplace = new string[] { "-", ",", ".", "!", "?" };
+
+                using (var writer = new StreamWriter(@"files\output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 0)
+                        {
+                            foreach (var symbol in symbolsToReplace)
+                            {
+                                if (line.Contains(symbol))
+                                {
+                                    line = line.Replace(symbol, "@");
+                                }
+                            }
+
+                            line = string.Join(" ", line.Split(" ").Reverse());
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+
+            /* Another way for reversing words
+            string sentence = "My name is Archit Patel";
+            StringBuilder sb = new StringBuilder();
+            string[] split = sentence.Split(' ');
+
+            for (int i = split.Length - 1; i > -1; i--)
+            {
+                sb.Append(split[i]);
+                sb.Append(" ");
+            }
+
+            Console.WriteLine(sb);
+            */
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/StreamsFilesAndDirectoriesExercise/06ZipAndExtract/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO.Compression;
+
+namespace _06ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startPath = @"files\images";
+            string zipPath = @"files\result.zip";
+            string extractPath = @"files\extract";
+
+            ZipFile.CreateFromDirectory(startPath, zipPath, CompressionLevel.Fastest, true);
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+    }
+}
