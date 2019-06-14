204fc017b35f420ebd2c78f85f6e1949d99ce7da
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-14 22:59:33 +0300

    7/10 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/MultidimensionalArraysExercise/04MatrixShuffling/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace _04MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] values = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = values[col];
+                }
+            }
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+
+                if (command != "swap" || tokens.Length != 5)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                int firstRow = int.Parse(tokens[1]);
+                int firstCol = int.Parse(tokens[2]);
+
+                int secondRow = int.Parse(tokens[3]);
+                int secondCol = int.Parse(tokens[4]);
+
+                if (firstRow < 0 || firstCol < 0 || secondRow < 0 || secondCol < 0
+                    || firstRow >= rows || firstCol >= cols || secondRow >= rows || secondCol >= cols)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                string temp = matrix[firstRow, firstCol];
+                matrix[firstRow, firstCol] = matrix[secondRow, secondCol];
+                matrix[secondRow, secondCol] = temp;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        Console.Write($"{matrix[row, col]} ");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
