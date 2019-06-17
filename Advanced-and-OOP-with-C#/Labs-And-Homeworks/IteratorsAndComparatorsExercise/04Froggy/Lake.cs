173d7ab7d16f1319bd88445bd940dfe01ac02e2c
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-17 18:16:02 +0300

    3/7 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/IteratorsAndComparatorsExercise/04Froggy/Lake.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparatorsExercise
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] stones;
+
+        public Lake(int[] stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int position = 0; position < stones.Length; position++)
+            {
+                if (position % 2 == 0)
+                {
+                    yield return stones[position];
+                }
+            }
+
+            for (int position = this.stones.Length - 1; position >= 0; position--)
+            {
+                if (position % 2 != 0)
+                {
+                    yield return stones[position];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
