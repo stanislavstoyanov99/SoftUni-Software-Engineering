8e7fafd10406ff57b100fb4d5622e9ac57d96904
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-18 10:13:24 +0300

    90/100 3rd problem done!

---
+++ Advanced-and-OOP-with-C#/Exams/CSharpAdvancedExam24Feb19/03Heroes/Hero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes
+{
+    public class Hero
+    {
+        public Hero(string name, int level, Item item)
+        {
+            this.Name = name;
+            this.Level = level;
+            this.Item = item;
+        }
+
+        public string Name { get; set; }
+
+        public int Level { get; set; }
+
+        public Item Item { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Hero: {this.Name} – {this.Level}lvl");
+            sb.AppendLine("Item:");
+            sb.AppendLine($"  * Strength: {this.Item.Strenght}");
+            sb.AppendLine($"  * Ability: {this.Item.Ability}");
+            sb.Append($"  * Intelligence: {this.Item.Intelligence}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
