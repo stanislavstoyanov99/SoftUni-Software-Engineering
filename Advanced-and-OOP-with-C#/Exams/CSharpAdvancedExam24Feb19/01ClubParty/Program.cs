0158a7a4d01e3e94c07a3c6dc3ea2b154fd89f4e
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-18 11:20:40 +0300

    1/3 problems done!

---
+++ Advanced-and-OOP-with-C#/Exams/CSharpAdvancedExam24Feb19/01ClubParty/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01ClubParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxCapacity = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Stack<string> symbols = new Stack<string>(input);
+
+            List<string> halls = new List<string>();
+            List<int> people = new List<int>();
+
+            while (symbols.Any())
+            {
+                string currentSymbol = symbols.Pop();
+                int currentPeople = 0;
+
+                bool isNumber = int.TryParse(currentSymbol, out currentPeople);
+
+                if (isNumber)
+                {
+                    if (halls.Count != 0)
+                    {
+                        if (currentPeople + people.Sum() <= maxCapacity)
+                        {
+                            people.Add(currentPeople);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{halls[0]} -> {string.Join(", ", people)}");
+                            halls.RemoveAt(0);
+                            people.Clear();
+
+                            if (halls.Count != 0)
+                            {
+                                people.Add(currentPeople);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    halls.Add(currentSymbol);
+                }
+            }
+        }
+    }
+}
