9a0c6ec9c559f7aef19f599e32e5f10a817b16f8
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-30 16:40:51 +0300

    Added lab

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/WorkingWithAbstractionLab/01RhombusOfStars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01RhombusOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int figureSize = int.Parse(Console.ReadLine());
+
+            for (int starsCount = 1; starsCount <= figureSize; starsCount++)
+            {
+                PrintRow(figureSize, starsCount);
+            }
+
+            for (int starsCount = figureSize - 1; starsCount >= 1; starsCount--)
+            {
+                PrintRow(figureSize, starsCount);
+            }
+        }
+
+        private static void PrintRow(int figureSize, int starsCount)
+        {
+            for (int i = 0; i < figureSize - starsCount; i++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int col = 1; col < starsCount; col++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.WriteLine("*");
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/WorkingWithAbstractionLab/02PointInRectangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02PointInRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
