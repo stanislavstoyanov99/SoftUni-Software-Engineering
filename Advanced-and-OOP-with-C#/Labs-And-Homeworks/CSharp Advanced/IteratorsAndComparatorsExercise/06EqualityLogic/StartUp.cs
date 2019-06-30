0293443baa30bb8451d8610c3fa1793c646eadd8
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-30 12:09:39 +0300

    7/7 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp Advanced/IteratorsAndComparatorsExercise/06EqualityLogic/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            SortedSet<Person> sortedPeople = new SortedSet<Person>();
+            HashSet<Person> hashsetPeople = new HashSet<Person>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                Person person = new Person(name, age);
+
+                sortedPeople.Add(person);
+                hashsetPeople.Add(person);
+            }
+
+            Console.WriteLine(sortedPeople.Count);
+            Console.WriteLine(hashsetPeople.Count);
+        }
+    }
+}
