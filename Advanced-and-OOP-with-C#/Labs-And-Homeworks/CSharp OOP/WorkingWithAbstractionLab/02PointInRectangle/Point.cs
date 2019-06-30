c40e52d11485dd82092e2878517e9b84c6c2cdcf
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-30 17:14:06 +0300

    2/4 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/WorkingWithAbstractionLab/02PointInRectangle/Point.cs
@@ -0,0 +1,15 @@
+﻿namespace PointInRectangle
+{
+    public class Point
+    {
+        public Point(int coordinateX, int coordinateY)
+        {
+            this.CoordinateX = coordinateX;
+            this.CoordinateY = coordinateY;
+        }
+
+        public int CoordinateX { get; set; }
+
+        public int CoordinateY { get; set; }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/WorkingWithAbstractionLab/02PointInRectangle/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿namespace PointInRectangle
+{
+    public class Rectangle
+    {
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            this.TopLeft = topLeft;
+            this.BottomRight = bottomRight;
+        }
+
+        public Point TopLeft { get; set; }
+
+        public Point BottomRight { get; set; }
+
+        public bool Contains(Point point)
+        {
+            bool result = false;
+
+            if (this.TopLeft.CoordinateX <= point.CoordinateX
+                && this.BottomRight.CoordinateX >= point.CoordinateX
+                && this.TopLeft.CoordinateY <= point.CoordinateY
+                && this.BottomRight.CoordinateY >= point.CoordinateY)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
