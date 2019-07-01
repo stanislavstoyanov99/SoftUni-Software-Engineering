78a2f00f88d6a199ea04f76e04ba9e67908577fd
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-02 00:30:21 +0300

    Updated version

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/WorkingWithAbstractionLab/02PointInRectangle/Engine.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace PointInRectangle
+{
+    public class Engine
+    {
+        private int[] rectangleCoordinates;
+        private int[] pointCoordinates;
+
+        public Engine()
+        {
+            this.rectangleCoordinates = new int[] { };
+            this.pointCoordinates = new int[] { };
+        }
+
+        public void Start()
+        {
+            rectangleCoordinates = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Point topLeftPoint = this.CreateTopLeftPoints();
+            Point bottomRightPoint = this.CreateBottomRightPoints();
+
+            Rectangle rectangle = new Rectangle(topLeftPoint, bottomRightPoint);
+
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int line = 1; line <= numberOfLines; line++)
+            {
+                pointCoordinates = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                Point point = this.CreatePoint();
+
+                PrintResult(rectangle, point);
+            }
+        }
+
+        private void PrintResult(Rectangle rectangle, Point point)
+        {
+            Console.WriteLine(rectangle.Contains(point));
+        }
+
+        private Point CreatePoint()
+        {
+            int coordinateX = pointCoordinates[0];
+            int coordinateY = pointCoordinates[1];
+
+            Point point = new Point(coordinateX, coordinateY);
+
+            return point;
+        }
+
+        private Point CreateBottomRightPoints()
+        {
+            int bottomRightX = rectangleCoordinates[2];
+            int bottomRightY = rectangleCoordinates[3];
+
+            Point point = new Point(bottomRightX, bottomRightY);
+
+            return point;
+        }
+
+        private Point CreateTopLeftPoints()
+        {
+            int topLeftX = rectangleCoordinates[0];
+            int topleftY = rectangleCoordinates[1];
+
+            Point point = new Point(topLeftX, topleftY);
+
+            return point;
+        }
+    }
+}
--- Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/WorkingWithAbstractionLab/02PointInRectangle/StartUp.cs
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/WorkingWithAbstractionLab/02PointInRectangle/StartUp.cs
@@ -1,42 +1,11 @@
-﻿using System;
-using System.Linq;
-
-namespace PointInRectangle
+﻿namespace PointInRectangle
 {
-    class StartUp
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            int[] rectangleCoordinates = Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToArray();
-
-            int topLeftX = rectangleCoordinates[0];
-            int topLeftY = rectangleCoordinates[1];
-            Point topLeftPoint = new Point(topLeftX, topLeftY);
-
-            int bottomRightX = rectangleCoordinates[2];
-            int bottomRightY = rectangleCoordinates[3];
-            Point bottomRightPoint = new Point(bottomRightX, bottomRightY);
-
-            Rectangle rectangle = new Rectangle(topLeftPoint, bottomRightPoint);
-
-            int numberOfLines = int.Parse(Console.ReadLine());
-            for (int line = 1; line <= numberOfLines; line++)
-            {
-                int[] pointCoordinates = Console.ReadLine()
-                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse)
-                    .ToArray();
-
-                int coordinateX = pointCoordinates[0];
-                int coordinateY = pointCoordinates[1];
-
-                Point point = new Point(coordinateX, coordinateY);
-
-                Console.WriteLine(rectangle.Contains(point));
-            }
+            Engine engine = new Engine();
+            engine.Start();
         }
     }
 }
