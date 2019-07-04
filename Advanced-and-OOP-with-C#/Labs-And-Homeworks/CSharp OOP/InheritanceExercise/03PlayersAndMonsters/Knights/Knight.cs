b6242964f85cc7affa2ed3bac22f944ba20e920f
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-04 23:13:07 +0300

    3/6 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/InheritanceExercise/03PlayersAndMonsters/Knights/Knight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Knights
+{
+    public class Knight : Hero
+    {
+        public Knight(string username, int level) 
+            : base(username, level)
+        {
+
+        }
+    }
+}
