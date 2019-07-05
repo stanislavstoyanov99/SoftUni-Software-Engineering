0140978861cae65933e374b930f25122ae4f03f4
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-05 14:58:31 +0300

    4/5 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/InheritanceLab/04RandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rand;
+
+        public RandomList()
+        {
+            this.rand = new Random();
+        }
+
+        public string RandomString()
+        {
+            int index = rand.Next(0, this.Count);
+            string elementToRemove = this[index];
+            this.RemoveAt(index);
+
+            return elementToRemove;
+        }
+    }
+}
