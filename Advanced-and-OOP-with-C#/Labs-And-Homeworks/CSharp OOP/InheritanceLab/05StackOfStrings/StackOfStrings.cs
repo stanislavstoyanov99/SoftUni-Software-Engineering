c468384f87829b656fac58874ccf847cb5245855
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-05 14:58:48 +0300

    5/5 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/InheritanceLab/05StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    // Generic version
+    public class StackOfStrings<T> : Stack<T>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            foreach (var element in collection)
+            {
+                this.Push(element);
+            }
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/InheritanceLab/05StackOfStrings/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "Stefan", "Pesho", "Kiro" };
+
+            var stack = new StackOfStrings<string>();
+            stack.AddRange(names);
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
