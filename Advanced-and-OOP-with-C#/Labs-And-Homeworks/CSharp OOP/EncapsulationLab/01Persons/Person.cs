bee292d3b3e6d35b6a33c99153ef175eb59403c1
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-07 22:58:46 +0300

    Added encapsulation lab

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/EncapsulationLab/01Persons/Person.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value > 0)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.FirstName} {this.LastName} is {this.Age} years old.");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/EncapsulationLab/01Persons/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var personInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = personInfo[0];
+                string lastName = personInfo[1];
+                int age = int.Parse(personInfo[2]);
+
+                var person = new Person(firstName, lastName, age);
+
+                people.Add(person);
+            }
+
+            var filteredPeople = people
+                .OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList();
+
+            foreach (var person in filteredPeople)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
