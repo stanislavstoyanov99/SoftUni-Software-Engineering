98318336afbc16445b7ffe42c0e820077ee59143
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-08 23:24:47 +0300

    2/4 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/EncapsulationLab/02Salary/Core/Engine.cs
@@ -0,0 +1,62 @@
+﻿namespace PersonsInfo.Core
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Engine
+    {
+        private List<Person> people;
+
+        public Engine()
+        {
+            this.people = new List<Person>();
+        }
+
+        public void Run()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = CreatePerson(personInfo);
+
+                people.Add(person);
+            }
+
+            decimal percentageToIncrease = decimal.Parse(Console.ReadLine());
+            IncreaseSalary(percentageToIncrease);
+
+            PrintPeople();
+        }
+
+        private Person CreatePerson(string[] personInfo)
+        {
+            string personFirstName = personInfo[0];
+            string personLastName = personInfo[1];
+            int personAge = int.Parse(personInfo[2]);
+            decimal personSalary = decimal.Parse(personInfo[3]);
+
+            Person person = new Person(personFirstName, personLastName, personAge, personSalary);
+            return person;
+        }
+
+        private void IncreaseSalary(decimal percentageToIncrease)
+        {
+            foreach (var person in people)
+            {
+                person.IncreaseSalary(percentageToIncrease);
+            }
+        }
+
+        private void PrintPeople()
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/EncapsulationLab/02Salary/Models/Person.cs
@@ -0,0 +1,72 @@
+﻿namespace PersonsInfo
+{
+    using System.Text;
+
+    public class Person
+    {
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value > 0)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if (value > 0)
+                {
+                    this.salary = value;
+                }
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
