80359a18fadb7187b0b11bbce572688f4d19e634
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-09 13:27:03 +0300

    3/4 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/EncapsulationLab/03Validation/Core/Engine.cs
@@ -0,0 +1,68 @@
+﻿namespace PersonsInfo.Core
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Engine
+    {
+        private List<Person> people;
+
+        public Engine()
+        {
+            this.people = new List<Person>();
+        }
+
+        public void Run()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                AddPersonToCollection(personInfo);
+            }
+
+            decimal percentageToIncrease = decimal.Parse(Console.ReadLine());
+
+            IncreaseSalary(percentageToIncrease);
+
+            PrintPeople();
+        }
+
+        private void IncreaseSalary(decimal percentageToIncrease)
+        {
+            foreach (var person in people)
+            {
+                person.IncreaseSalary(percentageToIncrease);
+            }
+        }
+
+        private void AddPersonToCollection(string[] personInfo)
+        {
+            try
+            {
+                string firstName = personInfo[0];
+                string lastName = personInfo[1];
+                int age = int.Parse(personInfo[2]);
+                decimal salary = decimal.Parse(personInfo[3]);
+
+                Person person = new Person(firstName, lastName, age, salary);
+                people.Add(person);
+            }
+            catch (ArgumentException aex)
+            {
+                Console.WriteLine(aex.Message);
+            }
+        }
+
+        private void PrintPeople()
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/EncapsulationLab/03Validation/Exceptions/ExceptionMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonsInfo.Exceptions
+{
+    public static class ExceptionMessages
+    {
+        public static string ZeroOrNegativeAgeException = "Age cannot be zero or a negative integer!";
+
+        public static string FirstNameException = "First name cannot contain fewer than 3 symbols!";
+
+        public static string LastNameException = "Last name cannot contain fewer than 3 symbols!";
+
+        public static string SalaryException = "Salary cannot be less than 460 leva!";
+    }
+}
