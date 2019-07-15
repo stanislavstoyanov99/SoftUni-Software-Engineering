08d573ca67812664b957620bd9111b9e14b06a94
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-15 12:08:04 +0300

    1/2 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/InterfacesAndAbstractionLab/01Shapes/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Shapes.Interfaces;
+using Shapes.Models;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            IDrawable rectangle = new Rectangle(width, height);
+
+            circle.Draw();
+            rectangle.Draw();
+        }
+    }
+}
