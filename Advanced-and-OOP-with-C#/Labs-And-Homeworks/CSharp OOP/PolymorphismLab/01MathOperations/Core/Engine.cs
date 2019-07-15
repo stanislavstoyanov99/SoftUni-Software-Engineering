df0290241d60fd08068eac5b729aecd083977617
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-16 00:21:22 +0300

    1/3 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/PolymorphismLab/01MathOperations/Core/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Operations.Models;
+
+namespace Operations.Core
+{
+    public class Engine
+    {
+        private readonly MathOperations mathOperations;
+
+        public Engine()
+        {
+            this.mathOperations = new MathOperations();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(mathOperations.Add(2, 3));
+            Console.WriteLine(mathOperations.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mathOperations.Add(2.2m, 3.3m, 4.4m));
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/PolymorphismLab/01MathOperations/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using Operations.Core;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
