31d19ef3fc11129690530e01a3523e1d243d2ff9
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-16 11:13:10 +0300

    2/3 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/PolymorphismLab/02Animals/Core/Engine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Animals.Factories;
+using Animals.Models;
+
+namespace Animals.Core
+{
+    public class Engine
+    {
+        private readonly AnimalFactory animalFactory;
+
+        public Engine()
+        {
+            this.animalFactory = new AnimalFactory();
+        }
+
+        public void Run()
+        {
+            Animal cat = this.animalFactory.CreateAnimal("Pesho", "Whiskas", "Cat");
+            Animal dog = this.animalFactory.CreateAnimal("Gosho", "Meat", "Dog");
+
+            PrintAnimals(cat, dog);
+        }
+
+        private void PrintAnimals(Animal cat, Animal dog)
+        {
+            Console.WriteLine(cat.ExplainSelf());
+
+            Console.WriteLine(dog.ExplainSelf());
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/PolymorphismLab/02Animals/Factories/AnimalFactory.cs
@@ -0,0 +1,23 @@
+﻿using Animals.Models;
+
+namespace Animals.Factories
+{
+    public class AnimalFactory
+    {
+        public Animal CreateAnimal(string name, string favouriteFood, string type)
+        {
+            Animal animal = null;
+
+            if (type == "Cat")
+            {
+                animal = new Cat(name, favouriteFood);
+            }
+            else if (type == "Dog")
+            {
+                animal = new Dog(name, favouriteFood);
+            }
+
+            return animal;
+        }
+    }
+}
