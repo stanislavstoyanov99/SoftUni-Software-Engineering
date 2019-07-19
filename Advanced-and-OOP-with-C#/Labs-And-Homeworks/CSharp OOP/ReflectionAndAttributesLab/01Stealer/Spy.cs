6fe02334d4567cc4dd6610e92ec1f9535fe27798
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-19 19:43:22 +0300

    1/6 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/ReflectionAndAttributesLab/01Stealer/Spy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+public class Spy
+{
+    public string StealFieldInfo(string className, params string[] classFields)
+    {
+        Type type = Type.GetType(className);
+
+        FieldInfo[] fields = type.GetFields(
+            BindingFlags.Instance |
+            BindingFlags.Public |
+            BindingFlags.NonPublic |
+            BindingFlags.Static);
+
+        StringBuilder sb = new StringBuilder();
+
+        Object classInstance = Activator.CreateInstance(type, new object[] { });
+
+        sb.AppendLine($"Class under investigation: {className}");
+
+        var filteredFields = fields
+            .Where(f => classFields.Contains(f.Name))
+            .ToList();
+
+        foreach (FieldInfo field in filteredFields)
+        {
+            sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
