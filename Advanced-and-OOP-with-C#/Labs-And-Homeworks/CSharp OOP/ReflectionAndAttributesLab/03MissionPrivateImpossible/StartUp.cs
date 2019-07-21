cf3517756a40cd8aeea2e3db88b26cf076dd0da7
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-21 16:38:15 +0300

    3/6 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/ReflectionAndAttributesLab/03MissionPrivateImpossible/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03MissionPrivateImpossible
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.RevealPrivateMethods("Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+}
