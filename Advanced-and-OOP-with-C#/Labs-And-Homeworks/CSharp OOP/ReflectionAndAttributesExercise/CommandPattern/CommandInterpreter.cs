24d04580c99e7b04e34c6396a993fab42e39e9b1
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-22 23:49:26 +0300

    1/2 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/ReflectionAndAttributesExercise/CommandPattern/CommandInterpreter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string COMMAND_POSTFIX = "Command";
+
+        public string Read(string inputLine)
+        {
+            string[] commandTokes = inputLine
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = commandTokes[0] + COMMAND_POSTFIX;
+            string[] commandArgs = commandTokes
+                .Skip(1)
+                .ToArray();
+
+            Type type = Assembly.GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == commandName);
+
+            if (type == null)
+            {
+                throw new NullReferenceException("type is missing!");
+            }
+
+            ICommand instance = (ICommand)Activator.CreateInstance(type);
+
+            string result = instance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/ReflectionAndAttributesExercise/CommandPattern/Core/Engine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string inputLine = Console.ReadLine();
+
+                string result = this.commandInterpreter.Read(inputLine);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
