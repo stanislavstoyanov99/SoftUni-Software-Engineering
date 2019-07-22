77b3188d18fb8b4c37580e339560983acec35f0f
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-22 23:51:08 +0300

    6/6 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/ReflectionAndAttributesLab/06CodeTracker/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        Type type = typeof(StartUp);
+        var methods = type.GetMethods(
+            BindingFlags.Instance | 
+            BindingFlags.Static | 
+            BindingFlags.Public);
+
+        foreach (MethodInfo method in methods)
+        {
+            if (method.CustomAttributes.Any(m => m.AttributeType == typeof(AuthorAttribute)))
+            {
+                var attributes = method.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute attribute in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                }
+            }
+        }
+    }
+}
