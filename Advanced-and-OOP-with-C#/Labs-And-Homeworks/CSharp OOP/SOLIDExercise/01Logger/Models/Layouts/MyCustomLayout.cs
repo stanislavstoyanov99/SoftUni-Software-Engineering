459949a0f9b31bcf9c201e4628d11e7ee16b350f
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-23 23:39:43 +0300

    Extended project with custom layout

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/SOLIDExercise/01Logger/Models/Layouts/MyCustomLayout.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+using _01Logger.Models.Contracts;
+
+namespace _01Logger.Models.Layouts
+{
+    class MyCustomLayout : ILayout
+    {
+        public string Format => GetFormat();
+
+        private string GetFormat()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("My custom format:")
+                .AppendLine("Date: {0}")
+                .AppendLine("Level: {1}")
+                .AppendLine("Message: {2}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
