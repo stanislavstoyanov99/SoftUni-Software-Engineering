d954d1285117b5ba2e35e1d1520009ef19487569
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-28 23:16:19 +0300

    2/7 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/ExceptionsAndErrorHandlingLab/02EnterNumbers/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02EnterNumbers
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    ReadNumber(1, 10);
+                }
+                catch (ArgumentException)
+                {
+                    i = 0;
+                    ReadNumber(1, 10);
+                }
+            }
+        }
+
+        public static void ReadNumber(int start, int end)
+        {
+            Console.Write("Enter number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input.GetType() != typeof(int))
+            {
+                throw new ArgumentException("The input is not a number");
+            }
+
+            if (input < start || input > end)
+            {
+                throw new ArgumentException($"The number is not in the given range [{start}]-[{end}]");
+            }
+        }
+    }
+}
