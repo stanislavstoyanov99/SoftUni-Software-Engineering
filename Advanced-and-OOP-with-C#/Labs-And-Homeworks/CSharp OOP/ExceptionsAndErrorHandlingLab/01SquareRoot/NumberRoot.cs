f0a3fcd61131174d4061715f7b65fa76d346f0f0
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-28 12:12:08 +0300

    1/7 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/ExceptionsAndErrorHandlingLab/01SquareRoot/NumberRoot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01SquareRoot
+{
+    public class NumberRoot
+    {
+        private int number;
+
+        public NumberRoot(int number)
+        {
+            this.Number = number;
+        }
+
+        public int Number
+        {
+            get => this.number;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid number");
+                }
+
+                this.number = value;
+            }
+        }
+
+        public void CalculateRoot(int number)
+        {
+            double numberRoot = Math.Sqrt(number);
+
+            Console.WriteLine($"Number root: {numberRoot:F3}");
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/ExceptionsAndErrorHandlingLab/01SquareRoot/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01SquareRoot
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            try
+            {
+                NumberRoot numberRoot = new NumberRoot(number);
+
+                numberRoot.CalculateRoot(number);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
