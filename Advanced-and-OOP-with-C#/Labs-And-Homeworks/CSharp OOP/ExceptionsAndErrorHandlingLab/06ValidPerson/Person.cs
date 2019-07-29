5db14762b2127342f24e8cdcc238b7f66b49a475
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-07-29 13:28:14 +0300

    6/7 problems done!

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/ExceptionsAndErrorHandlingLab/06ValidPerson/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _06ValidPerson
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName
+        {
+            get => this.firstName;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "The first name cannot be null or empty.");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => this.lastName;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "The last name cannot be null or empty.");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get => this.age;
+            private set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("value", "The age should be between 0 and 120");
+                }
+
+                this.age = value;
+            }
+        }
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/ExceptionsAndErrorHandlingLab/06ValidPerson/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _06ValidPerson
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person pesho = new Person("Pesho", "Goshev", 24);
+
+            try
+            {
+                Person noName = new Person(string.Empty, "Goshev", 31);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+
+            try
+            {
+                Person noLastName = new Person("Ivan", null, 63);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+
+            try
+            {
+                Person negativeAge = new Person("Stoyan", "Kolev", -1);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+
+            try
+            {
+                Person tooOldPerson = new Person("Iskren", "Ivanov", 121);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+        }
+    }
+}
