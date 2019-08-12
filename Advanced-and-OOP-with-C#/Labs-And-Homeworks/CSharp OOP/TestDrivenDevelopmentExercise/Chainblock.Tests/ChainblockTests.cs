af5f9de8450472b6a0d89e7159f25c0bad1a5425
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-08-12 22:26:58 +0300

    Added partly done exercise

---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/TestDrivenDevelopmentExercise/Chainblock.Tests/ChainblockTests.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+using System;
+
+using Chainblock.Models;
+using Chainblock.Contracts;
+
+namespace Chainblock.Tests
+{
+    [TestFixture]
+    public class ChainblockTests
+    {
+        private Chainblock.Models.Chainblock chainblock;
+        private Transaction transaction;
+
+        private const int id = 1;
+        private const TransactionStatus ts = TransactionStatus.Successfull;
+        private const string from = "Pesho";
+        private const string to = "Gosho";
+        private const double amount = 650;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.chainblock = new Models.Chainblock();
+            this.transaction = new Transaction(id, ts, from, to, amount);
+
+            // First add occurs here
+            this.chainblock.Add(this.transaction);
+        }
+
+        [Test]
+        public void Test_If_Add_Works_Correctly()
+        {
+            int expectedCount = 1;
+
+            Assert.AreEqual(expectedCount, this.chainblock.Count);
+        }
+
+        [Test]
+        public void Test_Adding_Same_Transaction_Twice()
+        {
+            Assert.Throws<InvalidOperationException>
+                (() => this.chainblock.Add(this.transaction));
+        }
+
+        [Test]
+        public void Test_If_ContainsBy_Transaction_Works_Correctly()
+        {
+            bool actualResult = this.chainblock.Contains(this.transaction);
+
+            Assert.IsTrue(actualResult);
+        }
+
+        [Test]
+        public void Test_If_Contains_Non_Existant_Transaction()
+        {
+            Transaction nonExistantTr = new Transaction(10, TransactionStatus.Unauthorized, from, to, amount);
+
+            bool result = this.chainblock.Contains(nonExistantTr);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Test_Contains_By_Id()
+        {
+            bool result = this.chainblock.Contains(id);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Test_Non_Existant_Id()
+        {
+            bool result = this.chainblock.Contains(5);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Test_Contains_With_Negative_Id()
+        {
+            Assert.Throws<InvalidOperationException>
+                (() => this.chainblock.Contains(-5));
+        }
+
+        [Test]
+        public void Test_If_Count_Works_Correctly()
+        {
+            int expectedCount = 2;
+
+            Transaction newTr = new Transaction(2, TransactionStatus.Failed, from, to, amount);
+
+            this.chainblock.Add(newTr);
+
+            Assert.AreEqual(expectedCount, this.chainblock.Count);
+        }
+
+        [Test]
+        public void Test_If_Get_By_Id_Works_Correctly()
+        {
+            ITransaction result = this.chainblock
+                .GetById(1);
+
+            Assert.AreSame(this.transaction, result);
+        }
+
+        [Test]
+        public void Test_Getting_Non_Existant_Transaction()
+        {
+            Assert.Throws<InvalidOperationException>
+                (() => this.chainblock.GetById(5));
+        }
+
+        [Test]
+        public void Test_If_Change_TransactionStatus_Works_Correctly()
+        {
+            TransactionStatus newStatus = TransactionStatus.Aborted;
+
+            this.chainblock.ChangeTransactionStatus(id, newStatus);
+
+            ITransaction result = this.chainblock.GetById(id);
+
+            Assert.AreEqual(newStatus, result.Status);
+        } 
+    }
+}
---
+++ Advanced-and-OOP-with-C#/Labs-And-Homeworks/CSharp OOP/TestDrivenDevelopmentExercise/Chainblock/Models/Transaction.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+using Chainblock.Contracts;
+
+namespace Chainblock.Models
+{
+    public class Transaction : ITransaction
+    {
+        private int id;
+        private TransactionStatus transactionStatus;
+        private string form;
+        private string to;
+        private double amount;
+
+        public Transaction(int id, TransactionStatus ts, string from, string to, double amount)
+        {
+            this.Id = id;
+            this.Status = ts;
+            this.From = from;
+            this.To = to;
+            this.Amount = amount;
+        }
+
+        public int Id
+        {
+            get => this.id;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Id value must be greater than zero.");
+                }
+
+                this.id = value;
+            }
+        }
+        public TransactionStatus Status
+        {
+            get => this.transactionStatus;
+            set
+            {
+                this.transactionStatus = value;
+            }
+        }
+        public string From
+        {
+            get => this.form;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+
+                this.form = value;
+            }
+        }
+        public string To
+        {
+            get => this.to;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+
+                this.to = value;
+            }
+        }
+        public double Amount
+        {
+            get => this.amount;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                this.amount = value;
+            }
+        }
+
+        public int CompareTo(ITransaction other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
