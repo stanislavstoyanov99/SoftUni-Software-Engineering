e944262dac253d7a0c828375f569d4feebcf477b
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-09-27 20:50:42 +0300

    Added modified query 6 and 7

--- DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/06. Built-in functions - Exercise/06FindTownsStartingWith.sql
+++ DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/06. Built-in functions - Exercise/06FindTownsStartingWith.sql
@@ -3,3 +3,9 @@
      FROM Towns
     WHERE [Name] LIKE '[MKBE]%'
  ORDER BY [Name]
+
+  SELECT TownId,
+         [Name]
+    FROM Towns
+   WHERE LEFT([Name], 1) IN ('M', 'K', 'B', 'E')
+ORDER BY [Name]
