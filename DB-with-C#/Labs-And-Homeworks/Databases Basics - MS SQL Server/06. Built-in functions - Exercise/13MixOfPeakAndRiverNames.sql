56cdcffdb052411e4af5275e50c6a34c3fcaa57c
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-09-27 20:51:35 +0300

    Added 13 query done! 1/1

---
+++ DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/06. Built-in functions - Exercise/13MixOfPeakAndRiverNames.sql
@@ -0,0 +1,19 @@
+  -- Without using Join
+  SELECT p.PeakName, r.RiverName,
+         LOWER (
+  	     CONCAT(LEFT(p.PeakName, LEN(p.PeakName) - 1), r.RiverName)
+  	     )
+  	  AS [Mix]
+    FROM Peaks AS p, Rivers AS r
+   WHERE RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
+ORDER BY [Mix]
+
+-- Using Join
+  SELECT p.PeakName, r.RiverName,
+         LOWER (
+  	     CONCAT(LEFT(p.PeakName, LEN(p.PeakName) - 1), r.RiverName)
+  	     )
+  	  AS [Mix]
+	FROM Peaks AS p
+	INNER JOIN Rivers AS r ON RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
+ORDER BY [Mix]
