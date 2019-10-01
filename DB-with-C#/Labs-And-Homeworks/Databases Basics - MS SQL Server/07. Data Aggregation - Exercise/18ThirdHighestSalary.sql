b9ebf7fcc23482c9115f607cde749413709d2f8c
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-10-01 23:01:00 +0300

    Added 17, 18 and 19 query done! 1/1

---
+++ DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/07. Data Aggregation - Exercise/18ThirdHighestSalary.sql
@@ -0,0 +1,16 @@
+  SELECT [ResultTable].DepartmentID,
+         [ResultTable].Salary
+	FROM (
+  SELECT DepartmentID,
+         Salary,
+         DENSE_RANK() OVER (
+		      PARTITION BY DepartmentID
+		          ORDER BY Salary 
+					  DESC
+						   ) 
+						AS [Rank]
+                      FROM Employees
+GROUP BY DepartmentID, Salary
+         ) 
+	  AS [ResultTable]
+   WHERE [Rank] = 3
