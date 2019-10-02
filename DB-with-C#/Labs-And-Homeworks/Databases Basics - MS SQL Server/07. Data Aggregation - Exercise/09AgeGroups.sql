2c7833b8157bcb754cf5efe308bf1307dbf898f3
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-10-02 19:36:25 +0300

    Added new solution to 09 query

--- DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/07. Data Aggregation - Exercise/09AgeGroups.sql
+++ DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/07. Data Aggregation - Exercise/09AgeGroups.sql
@@ -1,21 +1,47 @@
-   SELECT [Ages].ResultGroup 
+-- First solution   
+   SELECT [Ages].TemporaryTable 
 	   AS [AgeGroup],
-    COUNT ([Ages].ResultGroup) 
+    COUNT ([Ages].TemporaryTable) 
 	   AS [WizardCount]
      FROM 
 	      (
-   SELECT 
-	 CASE  
-	     WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
-		 WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
-		 WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
-		 WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
-		 WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
-		 WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
-		 WHEN Age >= 61 THEN '[61+]'
-	  END 
-	   AS [ResultGroup] 
-	 FROM WizzardDeposits
+           SELECT 
+	             CASE  
+	                 WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+	            	 WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+	            	 WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+	            	 WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+	            	 WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+	            	 WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+	            	 WHEN Age >= 61 THEN '[61+]'
+	              END 
+	               AS [TemporaryTable] 
+	             FROM WizzardDeposits
 	      )
 	   AS [Ages]
- GROUP BY [Ages].ResultGroup
+ GROUP BY [Ages].TemporaryTable
+
+ -- Second solution
+ SELECT 
+   CASE  
+	   WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+	   WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+	   WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+	   WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+	   WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+	   WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+	   WHEN Age >= 61 THEN '[61+]'
+	END 
+     AS [AgeGroup],
+  COUNT(*) AS [WizzardCount]
+         FROM WizzardDeposits
+     GROUP BY 
+         CASE  
+	         WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+	         WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+	         WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+	         WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+	         WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+	         WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+	         WHEN Age >= 61 THEN '[61+]'
+	      END
