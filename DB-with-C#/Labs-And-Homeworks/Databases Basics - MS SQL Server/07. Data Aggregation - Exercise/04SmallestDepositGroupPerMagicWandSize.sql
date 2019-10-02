9cdba0011d4bf01053be072e695884b9403f23e4
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-10-02 19:18:01 +0300

    Modified 04 query

--- DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/07. Data Aggregation - Exercise/04SmallestDepositGroupPerMagicWandSize.sql
+++ DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/07. Data Aggregation - Exercise/04SmallestDepositGroupPerMagicWandSize.sql
@@ -1,4 +1,17 @@
+-- First solution
 SELECT TOP(2) DepositGroup
       FROM    WizzardDeposits AS w
   GROUP BY    w.DepositGroup
   ORDER BY    AVG(MagicWandSize)
+
+-- Second solution
+SELECT DepositGroup 
+  FROM (
+		 SELECT TOP(2) DepositGroup,
+                AVG (MagicWandSize) AS [AverageWandSize]
+               FROM WizzardDeposits AS w
+           GROUP BY w.DepositGroup
+           ORDER BY [AverageWandSize]
+	   )
+    AS  [TemporaryTable]
+
