3941f13b88cc4f8803023567394df4f979ebaf17
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-10-02 19:59:36 +0300

    Added new solution to query 12

--- DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/07. Data Aggregation - Exercise/12RichWizardPoorWizard.sql
+++ DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/07. Data Aggregation - Exercise/12RichWizardPoorWizard.sql
@@ -1,3 +1,4 @@
+-- First Solution
 SELECT SUM([Result].Diff) 
     AS [SumDifference]
   FROM (
@@ -12,3 +13,16 @@ SELECT SUM([Result].Diff)
 		     AS wd
 	    )
 	AS [Result]
+
+-- Second Solution
+SELECT SUM([Difference]) 
+    AS [SumDifference] 
+  FROM (
+        SELECT FirstName AS [Host Wizard FirstName],
+               DepositAmount AS [Host Wizard Deposit Amount],
+        	   LEAD(FirstName) OVER (ORDER BY Id) AS [Guest Wizard],
+        	   LEAD(DepositAmount) OVER (ORDER BY Id) AS [Guest Wizard Deposit],
+        	   DepositAmount - LEAD(DepositAmount) OVER (ORDER BY Id) AS [Difference]
+          FROM WizzardDeposits
+	   ) 
+    AS DiffTable
