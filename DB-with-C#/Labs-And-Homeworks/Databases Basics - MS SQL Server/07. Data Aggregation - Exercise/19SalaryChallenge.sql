e28c73c56ae628576fd50195afeb77fd445e893e
Stanislav Stoyanov <45464117+stanislavstoyanov99@users.noreply.github.com>
2019-10-07 23:22:51 +0300

    Delete 19SalaryChallenge.sql

--- DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/07. Data Aggregation - Exercise/19SalaryChallenge.sql
+++
@@ -1,9 +0,0 @@
-SELECT TOP(10) FirstName,
-               LastName,
-	           DepartmentID
-      FROM     Employees AS e
-     WHERE     Salary > (SELECT AVG(Salary) 
-                           FROM Employees AS em
-				          WHERE em.DepartmentID = e.DepartmentID
-				        )
-  ORDER BY     DepartmentID
