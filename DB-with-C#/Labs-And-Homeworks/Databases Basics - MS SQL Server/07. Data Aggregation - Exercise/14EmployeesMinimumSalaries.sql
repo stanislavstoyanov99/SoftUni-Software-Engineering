46f23c8ad3faf66147bef2db0723b803e7cb6c61
Stanislav Stoyanov <45464117+stanislavstoyanov99@users.noreply.github.com>
2019-10-07 23:22:19 +0300

    Delete 14EmployeesMinimumSalaries.sql

--- DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/07. Data Aggregation - Exercise/14EmployeesMinimumSalaries.sql
+++
@@ -1,6 +0,0 @@
-  SELECT e.DepartmentID,
-     MIN (Salary) AS [MinimumSalary]
-    FROM Employees AS e
-   WHERE e.DepartmentID IN(2, 5, 7) 
-     AND e.HireDate > '2000-01-01'
-GROUP BY e.DepartmentID
