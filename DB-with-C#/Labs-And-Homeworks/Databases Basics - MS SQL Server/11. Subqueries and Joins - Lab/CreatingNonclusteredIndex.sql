c3a2a5b79bb8599afbf20a7d0a27d6e98a4a9cfd
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-10-10 15:54:10 +0300

    Added two queries

---
+++ DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/11. Subqueries and Joins - Lab/CreatingNonclusteredIndex.sql
@@ -0,0 +1,2 @@
+CREATE NONCLUSTERED INDEX IX_People_FirstName_LastName
+ON People(FirstName, LastName)
---
+++ DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/11. Subqueries and Joins - Lab/TempTableExample.sql
@@ -0,0 +1,7 @@
+CREATE TABLE #TempTable (
+Id INT PRIMARY KEY IDENTITY,
+[Name] NVARCHAR(50) NOT NULL,
+FacultyNumber INT NOT NULL
+)
+
+SELECT * FROM #TempTable
