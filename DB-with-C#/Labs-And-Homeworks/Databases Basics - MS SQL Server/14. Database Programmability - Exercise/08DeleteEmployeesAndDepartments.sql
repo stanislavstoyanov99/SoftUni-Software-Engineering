9508aa24626db7ddbaf009622913e882ae8852f5
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-10-16 11:35:31 +0300

    Modified 08 query

--- DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/14. Database Programmability - Exercise/08DeleteEmployeesAndDepartments.sql
+++ DB-with-C#/Labs-And-Homeworks/Databases Basics - MS SQL Server/14. Database Programmability - Exercise/08DeleteEmployeesAndDepartments.sql
@@ -1,36 +1,28 @@
 CREATE PROC usp_DeleteEmployeesFromDepartment(@departmentId INT)
 AS
-	 -- Delete employees from the employees projects which are in the given department
+     ALTER TABLE Departments ALTER COLUMN ManagerID INT NULL;
+
 	 DELETE FROM EmployeesProjects
 	 WHERE EmployeeID IN (SELECT e.EmployeeID
 	                        FROM Employees AS e 
 						   WHERE e.DepartmentID = @departmentId);
 
-	-- Set ManagerID to null for all managers in the given department
+	 UPDATE Departments
+	    SET ManagerID = NULL
+	  WHERE DepartmentID = @departmentId;
+
 	 UPDATE Employees
 	    SET ManagerID = NULL
 	  WHERE ManagerID IN (SELECT e.EmployeeID
 						    FROM Employees AS e
 						   WHERE e.DepartmentID = @departmentId);
 
-     -- Alter table Departments to allow ManagerID to be null
-     ALTER TABLE Departments 
-	 ALTER COLUMN ManagerID INT NULL;
-
-	 -- Set ManagerId to null in Departments table to remove the relationship between Departments and Employees
-	 UPDATE Departments
-	    SET ManagerID = NULL
-	  WHERE DepartmentID = @departmentId;
-
-	  -- Delete all employees in the given department
      DELETE FROM Employees
      WHERE DepartmentID = @departmentId;
 
-	 -- Delete all departments with the correspoding id
      DELETE FROM Departments
      WHERE DepartmentID = @departmentId;
 
-	 -- Show the count of all employees in the department (should be 0 if delete statements are written correctly)
      SELECT COUNT(e.EmployeeID)
      FROM Employees AS e
      WHERE e.DepartmentID = @departmentId;
