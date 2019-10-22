1be16ac7076e20375eadcb7e90feb5986e60e9ce
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-10-22 23:17:16 +0300

    Added first lab from Entity Framework Core

---
+++ DB-with-C#/Labs-And-Homeworks/Entity Framework Core/DBAppsIntroduction - Lab/DBAppsIntroduction/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace DBAppsIntroduction
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = @"Server=.\SQLEXPRESS;Database=SoftUni;Integrated Security=true";
+            SqlConnection dbConnection = new SqlConnection(connectionString);
+
+            dbConnection.Open();
+
+            using (dbConnection)
+            {
+                string sqlQuery = @"SELECT COUNT(*) FROM Employees";
+                SqlCommand sqlCommand = new SqlCommand(sqlQuery, dbConnection);
+
+                int employeesCount = (int)sqlCommand.ExecuteScalar();
+                Console.WriteLine($"Employees count: {employeesCount} employees from Employees table, SoftUni Database");
+            }
+        }
+    }
+}
---
+++ DB-with-C#/Labs-And-Homeworks/Entity Framework Core/DBAppsIntroduction - Lab/SqlDataReader/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace SqlDataReaderExample
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = @"Server=.\SQLEXPRESS;Database=SoftUni;Integrated Security=true";
+            SqlConnection dbConnection = new SqlConnection(connectionString);
+
+            dbConnection.Open();
+
+            using (dbConnection)
+            {
+                string sqlQuery = "SELECT * FROM Employees";
+                SqlCommand sqlCommand = new SqlCommand(sqlQuery, dbConnection);
+
+                using SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string firstName = (string)reader["FirstName"];
+                    string lastName = (string)reader["LastName"];
+                    decimal salary = (decimal)reader["Salary"];
+
+                    Console.WriteLine($"{firstName} {lastName} - {salary}");
+                }
+            }
+        }
+    }
+}
