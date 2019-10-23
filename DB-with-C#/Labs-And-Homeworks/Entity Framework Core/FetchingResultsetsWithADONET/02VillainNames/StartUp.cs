f4d50c0648c917612775945315cb9443f72782a7
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-10-23 22:55:14 +0300

    Added exercise 1-3 done!

---
+++ DB-with-C#/Labs-And-Homeworks/Entity Framework Core/FetchingResultsetsWithADONET/02VillainNames/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace _02VillainNames
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        private const string DB_NAME = "MinionsDB";
+
+        private static string connectionString = @$"Server=.\SQLEXPRESS;Database={DB_NAME};Integrated Security=true";
+
+        public static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            using (connection)
+            {
+                string queryText = @"SELECT [Name], 
+                                            COUNT(mv.MinionId) AS [MinionsCount]
+                                       FROM Villains AS v
+                                            JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+                                   GROUP BY [Name]
+                                     HAVING COUNT(mv.MinionId) > 3
+                                   ORDER BY COUNT(mv.MinionId) DESC";
+
+                SqlCommand sqlCommand = new SqlCommand(queryText, connection);
+
+                try
+                {
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    using (sqlDataReader)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            string villainName = (string)sqlDataReader["Name"];
+                            int minionsCount = (int)sqlDataReader["MinionsCount"];
+
+                            Console.WriteLine($"{villainName} - {minionsCount}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Invalid operation: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ DB-with-C#/Labs-And-Homeworks/Entity Framework Core/FetchingResultsetsWithADONET/03MinionNames/StartUp.cs
@@ -0,0 +1,75 @@
+﻿namespace _03MinionNames
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        private const string DB_NAME = "MinionsDB";
+
+        private static string connectionString = @$"Server=.\SQLEXPRESS;Database={DB_NAME};Integrated Security=true";
+
+        public static void Main(string[] args)
+        {
+            int villainId = int.Parse(Console.ReadLine());
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            using (connection)
+            {
+                string idQuery = @"SELECT [Name] FROM Villains WHERE Id = @Id";
+
+                using SqlCommand idCommand = new SqlCommand(idQuery, connection);
+
+                idCommand.Parameters.AddWithValue("@Id", villainId);
+
+                string villainName = (string)idCommand.ExecuteScalar();
+
+                if (villainName == null)
+                {
+                    Console.WriteLine($"No villain with ID {villainId} exists in the database.");
+                    return;
+                }
+
+                string minionsQuery = @"SELECT ROW_NUMBER() OVER(ORDER BY [Name]) AS RowNumber, 
+                                               m.[Name] AS MinionName, 
+                                               m.Age AS MinionAge
+                                          FROM MinionsVillains AS mv
+                                               JOIN Minions AS m ON m.Id = mv.MinionId
+                                         WHERE mv.VillainId = @Id
+                                      ORDER BY m.[Name]";
+
+                using SqlCommand minionsCommand = new SqlCommand(minionsQuery, connection);
+
+                minionsCommand.Parameters.AddWithValue("@Id", villainId);
+
+                SqlDataReader sqlDataReader = minionsCommand.ExecuteReader();
+
+                using (sqlDataReader)
+                {
+                    Console.WriteLine($"Villain: {villainName}");
+
+                    int counter = 1;
+                    while (sqlDataReader.Read())
+                    {
+                        string minionName = (string)sqlDataReader["MinionName"];
+
+                        if (sqlDataReader.HasRows)
+                        {
+                            int minionAge = (int)sqlDataReader["MinionAge"];
+
+                            Console.WriteLine($"{counter}. {minionName} {minionAge}");
+                            counter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("(no minions)");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
