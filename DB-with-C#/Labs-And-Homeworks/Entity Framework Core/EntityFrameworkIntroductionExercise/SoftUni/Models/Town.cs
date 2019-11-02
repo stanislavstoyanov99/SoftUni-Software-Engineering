3f4277f50b445e4a49f3b3d031979fc5f402f4ee
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-11-02 22:38:19 +0200

    Added exercise and problems 02-05 done! 100/100

---
+++ DB-with-C#/Labs-And-Homeworks/Entity Framework Core/EntityFrameworkIntroductionExercise/SoftUni/Models/Town.cs
@@ -0,0 +1,13 @@
+﻿namespace SoftUni.Models
+{
+    using System.Collections.Generic;
+
+    public class Town
+    {
+        public int TownId { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Address> Addresses { get; set; } = new HashSet<Address>();
+    }
+}
---
+++ DB-with-C#/Labs-And-Homeworks/Entity Framework Core/EntityFrameworkIntroductionExercise/SoftUni/StartUp.cs
@@ -0,0 +1,103 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using SoftUni.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var database = new SoftUniContext();
+
+            using (database)
+            {
+                Console.WriteLine(GetEmployeesFromResearchAndDevelopment(database));
+            }
+        }
+
+        // Problem - 03.	Employees Full Information
+        public static string GetEmployeesFullInformation(SoftUniContext db)
+        {
+            var employees = db.Employees
+                .Select(e => new
+                {
+                    EmployeeId = e.EmployeeId,
+                    FirstName = e.FirstName,
+                    MiddleName = e.MiddleName,
+                    LastName = e.LastName,
+                    JobTitle = e.JobTitle,
+                    Salary = e.Salary
+                })
+                .OrderBy(e => e.EmployeeId)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                string name = employee.FirstName;
+                string lastName = employee.LastName;
+                string middleName = employee.MiddleName;
+                string jobTitle = employee.JobTitle;
+                decimal salary = employee.Salary;
+
+                sb.AppendLine($"{name} {lastName} {middleName} {jobTitle} {salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        // Problem - 04.	Employees with Salary Over 50 000 
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext db)
+        {
+            var employees = db.Employees
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    Salary = e.Salary
+                })
+                .Where(e => e.Salary > 50_000)
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        // Problem - 05. Employees from Research and Development 
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext db)
+        {
+            var employees = db.Employees
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    DepartmentName = e.Department.Name,
+                    Salary = e.Salary
+                })
+                .Where(e => e.DepartmentName == "Research and Development")
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName} - ${employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
