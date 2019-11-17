bf78f577e843e3b2efadb9dbc42e5163a98cea5a
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-11-18 00:29:08 +0200

    Added exercise and project for homework

---
+++ DB-with-C#/Labs-And-Homeworks/Entity Framework Core/AutoMappingObjectsExercise/FastFood.Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿namespace FastFood.Web.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+
+    using FastFood.Web.ViewModels;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DB-with-C#/Labs-And-Homeworks/Entity Framework Core/AutoMappingObjectsExercise/FastFood.Web/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,71 @@
+﻿namespace FastFood.Web.MappingConfiguration
+{
+    using AutoMapper;
+    using System.Linq;
+
+    using Models;
+    using ViewModels.Positions;
+    using FastFood.Web.ViewModels.Items;
+    using FastFood.Web.ViewModels.Orders;
+    using FastFood.Web.ViewModels.Employees;
+    using FastFood.Web.ViewModels.Categories;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(src => src.PositionId, dest => dest.MapFrom(src => src.Id));
+
+            //Orders - WHY??
+            this.CreateMap<CreateOrderInputModel, Order>();
+
+            this.CreateMap<CreateOrderViewModel, Order>()
+                .ForMember(src => src.OrderItems, dest => dest.MapFrom(src => src.Items))
+                .ForMember(src => src.EmployeeId, dest => dest.MapFrom(src => src.Employees.Select(e => e)));
+
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(src => src.OrderId, dest => dest.MapFrom(src => src.Id))
+                .ForMember(src => src.Customer, dest => dest.MapFrom(src => src.Customer))
+                .ForMember(src => src.Employee, dest => dest.MapFrom(src => src.Employee.Name))
+                .ForMember(src => src.DateTime, dest => dest.MapFrom(src => src.DateTime.ToString()));
+
+            //Items
+            this.CreateMap<CreateItemInputModel, Item>()
+                .ForMember(src => src.Name, dest => dest.MapFrom(src => src.Name))
+                .ForMember(src => src.Price, dest => dest.MapFrom(src => src.Price))
+                .ForMember(src => src.CategoryId, dest => dest.MapFrom(src => src.CategoryId));
+
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(src => src.CategoryId, dest => dest.MapFrom(src => src.Id));
+
+            this.CreateMap<Item, ItemsAllViewModels>()
+                .ForMember(src => src.Name, dest => dest.MapFrom(src => src.Name))
+                .ForMember(src => src.Price, dest => dest.MapFrom(src => src.Price))
+                .ForMember(src => src.Category, dest => dest.MapFrom(src => src.Category));
+
+            //Employees - WHY??
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(src => src.Name, dest => dest.MapFrom(src => src.Name))
+                .ForMember(src => src.Age, dest => dest.MapFrom(src => src.Age))
+                .ForMember(src => src.Address, dest => dest.MapFrom(src => src.Address))
+                .ForMember(src => src.Position, dest => dest.MapFrom(src => src.Position.Name));
+
+            //Categories
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(src => src.Name, dest => dest.MapFrom(src => src.CategoryName));
+
+            this.CreateMap<Category, CategoryAllViewModel>()
+                .ForMember(src => src.Name, dest => dest.MapFrom(src => src.Name));
+        }
+    }
+}
