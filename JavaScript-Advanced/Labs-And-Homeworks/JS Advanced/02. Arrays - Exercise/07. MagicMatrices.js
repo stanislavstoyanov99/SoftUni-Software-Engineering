a91e232b99da1bee05fa0e4013ce32b28a572f02
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-06-08 09:22:44 +0300

    Update 07. MagicMatrices.js

--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/02. Arrays - Exercise/07. MagicMatrices.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/02. Arrays - Exercise/07. MagicMatrices.js
@@ -1,17 +1,19 @@
 function isMatrixMagical(matrix) {
+    let isMagic = true;
+    const firstRowSum = matrix[0].reduce((a, b) => a + b, 0);
+
     for (let row = 0; row < matrix.length; row++) {
         const rowSum = matrix[row].reduce((a, b) => a + b, 0);
-        let colSum = 0;
+        const colSum = matrix.map(x => x[row]).reduce((a, b) => a + b, 0);
 
-        for (let col = 0; col < matrix.length; col++) {
-            colSum += matrix[col][row];
+        if (rowSum !== firstRowSum || colSum !== firstRowSum) {
+            isMagic = false;
         }
-
-        colSum = 0;
-        return rowSum === colSum ? true : false;
     }
+
+    return isMagic;
 }
 
-// console.log(isMatrixMagical([[4, 5, 6], [6, 5, 4], [5, 5, 5]]));
-// console.log(isMatrixMagical([[11, 32, 45], [21, 0, 1], [21, 1, 1]]));
+console.log(isMatrixMagical([[4, 5, 6], [6, 5, 4], [5, 5, 5]]));
+console.log(isMatrixMagical([[11, 32, 45], [21, 0, 1], [21, 1, 1]]));
 console.log(isMatrixMagical([[1, 0, 0], [0, 0, 1], [0, 1, 0]]));
