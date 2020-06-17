01347cd0cef1c1cd8794609e8d85e3ddcfc45cdd
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-06-17 22:50:08 +0300

    4/9 problems done!

--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/04. DOM - Exercise/05. Chat-Room/solution.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/04. DOM - Exercise/05. Chat-Room/solution.js
@@ -1,5 +1,14 @@
 function solve() {
-   //TODO...
+   const textArea = document.querySelector('.chat-box-footer #chat_input');
+   const sendButton = document.querySelector('.chat-box-footer #send');
+   const messages = document.getElementById('chat_messages');
+
+   sendButton.addEventListener('click', () => {
+      const currMessage = document.createElement('div');
+      currMessage.setAttribute('class', 'message my-message');
+      currMessage.textContent = textArea.value;
+
+      messages.appendChild(currMessage);
+      textArea.value = '';
+   });
 }
-
-
