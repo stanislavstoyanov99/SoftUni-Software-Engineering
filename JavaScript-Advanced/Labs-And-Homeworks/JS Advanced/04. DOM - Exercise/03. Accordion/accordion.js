d84ea47bb8976813ee60b94392c06cbfc55aa634
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-06-17 19:53:54 +0300

    3/9 problems done!

--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/04. DOM - Exercise/03. Accordion/accordion.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/04. DOM - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    const button = document.querySelector('.head > .button');
+    const divExtra = document.querySelector('#extra');
+
+    if (divExtra.style.display === 'none' && button.textContent === 'More') {
+        divExtra.style.display = 'block';
+        button.textContent = 'Less';
+    } else {
+        divExtra.style.display = 'none';
+        button.textContent = 'More';
+    }
 }
