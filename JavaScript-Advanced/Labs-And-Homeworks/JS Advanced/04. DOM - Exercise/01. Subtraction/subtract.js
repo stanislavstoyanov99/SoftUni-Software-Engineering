c16027a8d4b05bca4965af0b263cd6fbdf4954e0
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-06-17 19:18:51 +0300

    1/9 problems done!

--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/04. DOM - Exercise/01. Subtraction/subtract.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/04. DOM - Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,11 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstNumberInput = document.getElementById('firstNumber');
+    const secondNumberInput = document.getElementById('secondNumber');
+
+    const firstNumber = Number(firstNumberInput.value);
+    const secondNumber = Number(secondNumberInput.value);
+
+    const result = firstNumber - secondNumber;
+    const resultDiv = document.querySelector('#result');
+    resultDiv.innerText = result.toString();
 }
