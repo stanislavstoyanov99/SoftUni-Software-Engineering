5fcfa425aaa25bfaa050e12f8f47ead3cd063d90
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-06-17 19:28:20 +0300

    2/9 problems done!

--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/04. DOM - Exercise/02. Fill-Dropdown/dropdown.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/04. DOM - Exercise/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    const textInput = document.getElementById('newItemText');
+    const valueInput = document.getElementById('newItemValue');
+
+    const menu = document.querySelector('#menu');
+
+    const optionElement = document.createElement('option');
+    optionElement.textContent = textInput.value;
+    optionElement.value = valueInput.value;
+
+    menu.appendChild(optionElement);
+
+    textInput.value = '';
+    valueInput.value = '';
 }
