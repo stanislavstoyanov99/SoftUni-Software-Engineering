38f5033f43c7a9c1e9885f69b107041556d40f02
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-06-18 12:40:18 +0300

    5/9 problems done!

--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/04. DOM - Exercise/04. Order-the-Names/solution.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/04. DOM - Exercise/04. Order-the-Names/solution.js
@@ -1,4 +1,40 @@
 function solve() {
+    const addButton = document.querySelector('#exercise button');
 
-    //TODO...
+    addButton.addEventListener('click', onClick);
+
+    const list = {};
+
+    // Fulfill list with uppercase letters
+    const items = document.querySelector('ol').querySelectorAll('li');
+
+    for (const item of [...items]) {
+        if (item.textContent.trim().length == 0) {
+            continue;
+        }
+
+        const letter = item.textContent[0].toUpperCase();
+        list[letter] = item.textContent;
+    }
+
+    function onClick() {
+        const inputField = document.querySelector('#exercise input');
+        const value = inputField.value;
+        
+        const firstLetter = value[0].toUpperCase();
+        const name = firstLetter + value.slice(1).toLowerCase();
+        
+        // Check if person name is already in the list
+        if (list.hasOwnProperty(firstLetter) == false) {
+            list[firstLetter] = name;
+        } else {
+            list[firstLetter] = list[firstLetter] + ', ' + name;
+        }
+
+        // DOM Manipulations
+        const index = firstLetter.charCodeAt(0) - 65;
+        items[index].textContent = list[firstLetter];
+
+        inputField.value = '';
+    }
 }
