f2e9603983422f1f5f3131206ceb759c873b136f
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-06-21 20:03:17 +0300

    7/7 problems done!

--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/05. DOM Manipulations - Lab/05. Mouse-Gradient/app.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/05. DOM Manipulations - Lab/05. Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    
+    const gradient = document.querySelector('#gradient');
+    const result = document.querySelector('#result');
+
+    function gradientMove(e) {
+        let power = e.offsetX / (e.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        result.textContent = power.toString().concat('%');
+    }
+
+    function gradientOut() {
+        result.textContent = "";
+    }
+
+    gradient.addEventListener('mousemove', gradientMove);
+    gradient.addEventListener('mouseout', gradientOut);
 }
--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/05. DOM Manipulations - Lab/07. Dynamic-Validation/app.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/05. DOM Manipulations - Lab/07. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+    const inputField = document.getElementById('email');
+
+    inputField.addEventListener('change', function() {
+        const inputValue = inputField.value;
+        inputField.classList.remove('error');
+        
+        if (inputValue.match(/^[a-z-\.\d]+@[a-z-\.]+\.[a-z]{2,4}/)) { 
+            return;
+        }
+
+        inputField.classList.add('error');
+    })
 }
