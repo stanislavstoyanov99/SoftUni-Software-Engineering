7a1bba82aefaa5473b87411e6285906c68dd517a
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-06-23 21:09:08 +0300

    Refactor problem 4 with one event listener.

--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/05. DOM Manipulations - Exercise/04. Locked-Profile/app.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/05. DOM Manipulations - Exercise/04. Locked-Profile/app.js
@@ -1,24 +1,26 @@
 function lockedProfile() {
-    const buttons = [...document.querySelectorAll('button')];
+    document.querySelector('#main').addEventListener('click', onClick);
 
-    buttons.forEach(btn => btn.addEventListener('click', (e) => {
-        const btn = e.target;
-        const { parentNode } = btn;
+    function onClick(e) {
+        if (e.target.nodeName === 'BUTTON') {
+            const btn = e.target;
+            const { parentNode } = btn;
+    
+            const lockStatus = parentNode.querySelector('input[type="radio"][value="lock"]');
+            const hiddenInput = [...parentNode.querySelectorAll('div')].filter(d => d.id.includes('HiddenFields'))[0];
 
-        const lockStatus = parentNode.querySelector('input[type="radio"]:checked').value;
-        const hiddenInput = parentNode.querySelector('div');
-
-        if (lockStatus === 'unlock') {
-            const btnContent = btn.textContent;
-
-            if (btnContent === 'Show more') {
-                hiddenInput.style.display = 'block';
-                btn.textContent = 'Hide it';
-            }
-            else if (btnContent === 'Hide it') {
-                hiddenInput.style.display = 'none';
-                btn.textContent = 'Show more';
+            if (!lockStatus.checked) {
+                const btnContent = btn.textContent;
+    
+                if (btnContent === 'Show more') {
+                    hiddenInput.style.display = 'block';
+                    btn.textContent = 'Hide it';
+                }
+                else if (btnContent === 'Hide it') {
+                    hiddenInput.style.display = 'none';
+                    btn.textContent = 'Show more';
+                }
             }
         }
-    }));
+    }
 }
