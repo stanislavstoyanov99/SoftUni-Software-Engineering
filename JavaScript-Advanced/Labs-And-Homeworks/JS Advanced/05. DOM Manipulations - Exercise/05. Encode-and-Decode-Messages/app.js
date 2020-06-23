2fed2d8a15c5d1d183b698b89a33febf1943a6df
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-06-23 16:09:39 +0300

    6/10 problems done!

--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/05. DOM Manipulations - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/05. DOM Manipulations - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,44 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const inputTextArea = document.querySelector("div > textarea:not(disabled)");
+    const outputTextArea = document.querySelector("div > textarea[disabled]");
+
+    const encodeBtn = inputTextArea.parentElement.querySelector("button");
+    const decodeBtn = outputTextArea.parentElement.querySelector("button");
+
+    encodeBtn.addEventListener('click', encode);
+    decodeBtn.addEventListener('click', decode);
+
+    function encode() {
+        encodedMessage = decryptor(inputTextArea.value, 'encode');
+
+        outputTextArea.value = encodedMessage;
+
+        inputTextArea.value = '';
+    }
+
+    function decode() {
+        const decodedMessage = decryptor(outputTextArea.value, 'decode');
+
+        outputTextArea.value = decodedMessage;
+    }
+
+    function decryptor(message, typeOfDecryption) {
+        let resultMessage = '';
+
+        for (let i = 0; i < message.length; i++) {
+            const letter = message[i];
+
+            let decryptedLetter = '';
+
+            if (typeOfDecryption === 'encode') {
+                decryptedLetter = letter.charCodeAt(0) + 1;
+            } else if (typeOfDecryption === 'decode') {
+                decryptedLetter = letter.charCodeAt(0) - 1;
+            }
+
+            resultMessage += String.fromCharCode(decryptedLetter);
+        }
+
+        return resultMessage;
+    }
 }
--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/05. DOM Manipulations - Exercise/06. Table-Search-Engine/solution.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/05. DOM Manipulations - Exercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-   //TODO...
+   const searchBtn = document.getElementById('searchBtn');
+
+   const tableData = document.querySelectorAll('.container tbody td');
+
+   let selectedRows = [];
+
+   searchBtn.addEventListener('click', () => {
+      if (selectedRows) {
+         selectedRows.forEach(row => row.parentElement.classList.remove('select'));
+      }
+
+      const inputField = document.getElementById('searchField');
+
+      for (let row = 0; row < tableData.length; row++) {
+         const currentRow = tableData[row];
+
+         if (currentRow.textContent.includes(inputField.value)) {
+            selectedRows.push(currentRow);
+            currentRow.parentElement.classList.add('select');
+         }
+      }
+      
+      inputField.value = '';
+   });
 }
