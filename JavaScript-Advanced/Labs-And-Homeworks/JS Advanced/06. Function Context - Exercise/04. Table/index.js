5fa43feaa5259b5d8f88d47c043604e491a9d750
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-06-24 23:48:59 +0300

    4/5 problems done!

--- JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/06. Function Context - Exercise/04. Table/index.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Advanced/06. Function Context - Exercise/04. Table/index.js
@@ -1,3 +1,23 @@
-function solve(){
-   // TODO
+function solve() {
+   const tableBody = document.querySelector('.minimalistBlack > tbody');
+
+   let selectedRow;
+   
+   tableBody.addEventListener('click', (e) => {
+      if (e.target.nodeName === 'TD') {
+         const target = e.target;
+
+         if (target.parentElement.style.backgroundColor) {
+            target.parentElement.style.backgroundColor = '';
+            return;
+         }
+
+         if (selectedRow) {
+            selectedRow.parentElement.style.backgroundColor = '';
+         }
+
+         target.parentElement.style.backgroundColor = '#413f5e';
+         selectedRow = target;
+      }
+   });
 }
