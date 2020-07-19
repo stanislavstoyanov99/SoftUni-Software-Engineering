c744d875ac85b7fb53cbe661a0c4bea1824ff6aa
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-07-19 23:50:06 +0300

    Finish myFurniture controller and make initialization of createFurniture controller.

--- JavaScript-Advanced/Labs-And-Homeworks/JS Applications/06. Routing - Lab/Furniture/controllers/createFurniture.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Applications/06. Routing - Lab/Furniture/controllers/createFurniture.js
@@ -1,1 +1,18 @@
-// TODO
+import * as data from '../api/data.js';
+
+const invalidInputMessage = 'This input value is invalid';
+const validInputMessage = 'This input value is valid';
+
+export default async function() {
+    await this.partial('./templates/createFurniture.hbs');
+
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        // TODO - make validation and take data from form
+        console.log(e.target);
+    });
+}
--- JavaScript-Advanced/Labs-And-Homeworks/JS Applications/06. Routing - Lab/Furniture/controllers/myFurniture.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Applications/06. Routing - Lab/Furniture/controllers/myFurniture.js
@@ -1,1 +1,16 @@
-// TODO
+import * as data from '../api/data.js';
+
+export default async function() {
+    let furniture = [];
+
+    try {
+        furniture = await data.getAllFurniture();
+    } catch (error) {
+        alert(`Error: ${error.message}`);
+        console.error(error);
+        return;    
+    }
+
+    const context = { furniture };
+    this.partial('./templates/mineFurniture.hbs', context);
+}
