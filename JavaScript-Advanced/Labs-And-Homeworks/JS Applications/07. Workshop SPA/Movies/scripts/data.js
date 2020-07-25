34d884d5b9614f7a4c13d68ca8ce6718f121553a
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-07-25 23:06:33 +0300

    Finish initial logic in data.js.

--- JavaScript-Advanced/Labs-And-Homeworks/JS Applications/07. Workshop SPA/Movies/scripts/data.js
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Applications/07. Workshop SPA/Movies/scripts/data.js
@@ -10,6 +10,7 @@ const api = {
     LOGIN: 'users/login',
     LOGOUT: 'users/logout',
     USERS: 'data/Users',
+    MOVIES: 'data/movies'
 };
 
 async function register(user) {
@@ -36,8 +37,74 @@ async function logout(token) {
     return await fetch(host(api.LOGOUT), {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',
             'user-token': token
         }
     });
+}
+
+async function createMovie(movie, token) {
+    return await (await fetch(host(api.MOVIES), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'user-token': token
+        },
+        body: JSON.stringify(movie)
+    })).json(); 
+}
+
+async function editMovie(editedMovie, id, token) {
+    return await (await fetch(host(api.MOVIES + '/' + id), {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'user-token': token
+        },
+        body: JSON.stringify(editedMovie)
+    })).json();
+}
+
+async function deleteMovie(id, token) {
+    return await (await fetch(host(api.MOVIES + '/' + id), {
+        method: 'DELETE',
+        headers: {
+            'user-token': token
+        }
+    })).json();
+}
+
+async function buyTicket(movie, token) {
+    const newTickets = movie.tickets - 1;
+    const movieId = movie.objectId;
+
+    return editMovie({ tickets: newTickets}, movieId, token);
+}
+
+async function getMovieByOwner(ownerId, token) {
+    return (await fetch(host(api.MOVIES + `where=ownerId%3D%27${ownerId}%27`), {
+        headers: {
+            'Content-Type': 'application/json',
+            'user-token': token
+        }
+    })).json();
+}
+
+async function getMovieById(id, token) {
+    return await (await fetch(host(api.movie + '/' + id), {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'user-token': token
+        }
+    })).json();
+}
+
+async function getAllMovies(token) {
+    return await (await fetch(host(api.MOVIES), {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'user-token': token
+        }
+    })).json();
 }
