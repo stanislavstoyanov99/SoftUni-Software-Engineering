f5804449b0dd839e22550d622ad64cf3678d48ed
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-07-30 23:22:40 +0300

    Problem 1 and 2 done!

---
+++ JavaScript-Advanced/Labs-And-Homeworks/JS Applications/09. Workshop Custom Components/01. button/app-btn.js
@@ -0,0 +1,33 @@
+class AppButton extends HTMLButtonElement {
+    static get observedAttributes() {
+        return ['btn-type', 'text'];
+    }
+
+    set btnType(newValue) {
+        if (this._btnType) {
+            this.classList.remove(this._btnType);
+        }
+
+        this.classList.add(newValue);
+        this._btnType = newValue;
+    }
+
+    set text(newValue) {
+        this.textContent = newValue;
+    }
+
+    constructor() {
+        super();
+        this.classList.add('btn');
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'btn-type') {
+            this.btnType = newValue;
+        } else if (name === 'text') {
+            this.text = newValue;
+        }
+    }
+}
+
+customElements.define('app-button', AppButton, { extends: 'button' });
